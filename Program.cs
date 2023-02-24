62209b5a9ff193822b26f6f9547b7945b357c504
Morgan Houston <mmhouston6@live.com>
2023-02-24 15:10:10 -0600

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿// System namespace needed to use Console class.
+using System;
+
+// My collection of classes for this project.
+namespace HelloWorld_Houston_Morgan
+{
+    // Main class for our program
+    class Program
+    {
+        // The Main method is executed when application starts
+        static void Main(string[] args)
+        {
+            // Asks the user for first name
+            Console.WriteLine("What is your first name?");
+            // Assigns user input to firstName variable
+            string firstName = Console.ReadLine();
+            // Asks the user for last name
+            Console.WriteLine("What is your last name?");
+            // Assigns user input to lastName variable
+            string lastName = Console.ReadLine();           
+
+            // Prints a welcome message with firstName and lastName
+            Console.WriteLine("Hello, " + firstName + " " + lastName + "!");
+        }
+    }
+}
